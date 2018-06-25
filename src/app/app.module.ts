1a5f5bb29598767b1015988464482e3868cc8065
Nick Matheis <nickmath21@gmail.com>
2018-06-25 19:15:55 -0400

    Worked on navbar and carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NgbModule, NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { JsonpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AboutusComponent } from './components/aboutus/aboutus.component';
-import { ServicesComponent } from './services/services.component';
+import { ServicesComponent } from './components/services/services.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 const appRoutes: Routes = [
   { path: 'aboutus', component: AboutusComponent },
@@ -15,13 +20,21 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     AboutusComponent,
-    ServicesComponent
+    ServicesComponent,
+    NavigationComponent,
+    CarouselComponent
+
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    NgbModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule,
+    JsonpModule
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [NgbCarouselConfig],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,22 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css'],
+  providers: [ NgbCarouselConfig ]
+})
+export class CarouselComponent {
+
+  @ViewChild('carousel') carousel: any;
+
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 5000;
+    config.wrap = false;
+    config.keyboard = false;
+  }
+}
+
+
