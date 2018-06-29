d53f9113adaf28bbf4f6687f7a7e72be5d798c67
Nick Matheis <nickmath21@gmail.com>
2018-06-28 22:23:50 -0400

    Improved the nav and made more responsive and fit carousel pics

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -14,7 +14,7 @@ export class CarouselComponent {
   constructor(config: NgbCarouselConfig) {
     // customize default values of carousels used by this component tree
     config.interval = 5000;
-    config.wrap = false;
+    config.wrap = true;
     config.keyboard = false;
   }
 }
